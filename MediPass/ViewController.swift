df6dd9683b2d0dfee97c26235d16e94c217db52b
kimdongjune <jun36817@naver.com>
2021-04-14 16:33:07 +0900

    Initial Commit

---
+++ MediPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediPass
+//
+//  Created by 김동준 on 2021/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
